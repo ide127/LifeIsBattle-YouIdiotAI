1fa19500d33b477053f7df383fe38aa35e63d816
Gonglee <a71825485@gmail.com>
2024-03-14 16:05:27 +0900

    Remove axios plugin that sets CSRF token in request headers

--- frontend/plugins/axios.js
+++
@@ -1,8 +0,0 @@
-export default function ({ $axios, app }) {
-	$axios.onRequest((config) => {
-		const csrfToken = app.$cookies.get("csrftoken");
-		if (csrfToken) {
-			config.headers.common["X-CSRFToken"] = csrfToken;
-		}
-	});
-}
